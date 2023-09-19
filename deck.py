8d802a67a0b902fb00eab49a1d292a9fa0425841
ColeCollector <colen827@deltalearns.ca>
2023-09-19 13:30:05 -0700

    Update deck.py

--- deck.py
+++ deck.py
@@ -2,26 +2,34 @@
 import random
 
 ranks = ['A','2','3','4','5','6','7','8','9','T','J','Q','K']
-suits = ['H','C','D','S']
+suits = ['H','C','S','D']
+banned = []
 
-deck = [f"{i} of {j}" for i in ranks for j in suits]
+deck = [f"{j}{i}" for i in suits for j in ranks]
 print(deck[0],deck[1],deck[2],deck[2],deck[3])
+fdeck = deck[]
+sdeck = deck[]
 
-random.shuffle(deck)
 
-ask = int(input("How many cards would you like to deal?\n"))
-hand = []
 
-for i in range(ask):
-    hand.append(deck[0])
-    deck.pop(0)
+def shuffle(deck):
+    random.shuffle(deck)
+    for i in banned:
+        deck.pop(i)
+    return deck
+
+def deal(deck):
+    ask = int(input("How many cards would you like to deal?\n"))
+    hand = []
+
+    for i in range(ask):
+        hand.append(deck[0])
+        banned.append(deck[0])
+        deck.pop(0)
 
 print(hand)
-deck.sort()
 print(deck)
 
-for i in range(deck):
-    if 
 
 #HCDS
 #a-k
