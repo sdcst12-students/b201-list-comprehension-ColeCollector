50dd4b8740fb02fa508af53faeccf7ce6466bb3a
ColeCollector <colen827@deltalearns.ca>
2023-09-21 11:48:58 -0700

    DONE

--- deck.py
+++ deck.py
@@ -3,19 +3,19 @@ import random
 
 ranks = ['A','2','3','4','5','6','7','8','9','T','J','Q','K']
 suits = ['H','C','S','D']
-banned = []
 
 deck = [f"{j}{i}" for i in suits for j in ranks]
-print(deck[0],deck[1],deck[2],deck[2],deck[3])
-fdeck = deck[]
-sdeck = deck[]
+print("The first five cards are: ",deck[0],deck[1],deck[2],deck[2],deck[3])
+z = round(len(deck)/2)
+fdeck = deck[0:z]
+sdeck = deck[z:]
+sdeck.reverse()
 
-
+deck = fdeck+sdeck
+print("The sorted deck is", deck)
 
 def shuffle(deck):
     random.shuffle(deck)
-    for i in banned:
-        deck.pop(i)
     return deck
 
 def deal(deck):
@@ -24,12 +24,9 @@ def deal(deck):
 
     for i in range(ask):
         hand.append(deck[0])
-        banned.append(deck[0])
         deck.pop(0)
-
-print(hand)
-print(deck)
+    print("You hand is:", hand)
 
 
-#HCDS
-#a-k
+shuffle(deck)
+deal(deck)
